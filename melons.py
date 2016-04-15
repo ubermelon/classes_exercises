9c7eb083bbdcdcc56505a606485abadf907deeaa
Rosie <rosie@riveter14-md.swna.wdpr.disney.com>
2016-04-15 15:30:20 -0700

    Finished part 3 of Classes Exercise

--- melons.py
+++ melons.py
@@ -24,28 +24,12 @@ class AbstractMelonOrder(object):
 
         self.shipped = True
 
+
 class DomesticMelonOrder(AbstractMelonOrder):
     """A domestic (in the US) melon order."""
 
-    # def __init__(self, species, qty):
-    #     """Initialize melon order attributes"""
-    #     self.species = species
-    #     self.qty = qty
-    #     self.shipped = False
     order_type = "domestic"
     tax = 0.08
-
-    # def get_total(self):
-    #     """Calculate price."""
-
-    #     base_price = 5
-    #     total = (1 + self.tax) * self.qty * base_price
-    #     return total
-
-    # def mark_shipped(self):
-    #     """Set shipped to true."""
-
-    #     self.shipped = True
 
 
 class InternationalMelonOrder(AbstractMelonOrder):
@@ -59,23 +43,26 @@ class InternationalMelonOrder(AbstractMelonOrder):
     order_type = "international"
     tax = 0.17
 
-    # def get_total(self):
-    #     """Calculate price."""
-    #     base_price = 5
-    #     # Christmas melons will cost 1.5 times base price.
-    #     if self.species == "Christmas melon":
-    #         base_price = base_price * 1.5
-    #     # a flat fee of $3 will be added to all international orders with less than 10 melons.
-
-    #     total = (1 + self.tax) * self.qty * base_price
-    #     return total
-
-    # def mark_shipped(self):
-    #     """Set shipped to true."""
-
-    #     self.shipped = True
-
     def get_country_code(self):
         """Return the country code."""
 
         return self.country_code
+
+class GovernmentMelonOrder(AbstractMelonOrder):
+    """A Government order that has no tax which includes inspection."""
+
+    def __init__(self, species, qty):
+        """Initialize locally country_code attributes"""
+        super(GovernmentMelonOrder, self).__init__(species, qty)
+        self.passed_inspection = False
+
+    order_type = "government"
+    tax = 0.0
+
+    def inspect_melons(self, passed):
+        """Set inspected to be true."""
+        if passed == True:
+            self.passed_inspection = True
+        else:
+            self.passed_inspection = False
+
